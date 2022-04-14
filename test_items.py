f7031252f2c62fb44e2432930d49d57927000d7b
FBS0\i.filimonov <i.filimonov@fbs.com>
2022-04-14 17:55:09 +0300

    search button

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#link = "http://selenium1py.pythonanywhere.com"
+
+
+def test_button_search(browser):
+    browser.get(link)
+    item = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    assert item, 'no item!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    time.sleep(5)
